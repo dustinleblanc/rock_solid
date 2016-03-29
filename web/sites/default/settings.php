8717223b536447ffe4d293a2a920b2f15f9df9b9
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-03-29 12:47:47 -0400

    Basic settings file / env setup

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -16,20 +16,24 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
  */
 include __DIR__ . "/settings.pantheon.php";
 
+$config_directories['sync'] = '../config/sync';
+$settings['install_profile'] = getenv('INSTALL_PROFILE');
+
 if (!isset($_ENV['PANTHEON_ENVIRONMENT'])) {
-    $settings['hash_salt'] = 'YtvcheoGbURHzhaAADkcNGZPBSaK5RX1dIpRb0B-JsyJhjiBC3BB0jXJr9iEbFZgnSiIepw7ZA';
-    $dotenv = new Dotenv\Dotenv('../');
-    $dotenv->load();
-    $databases['default']['default'] = [
-        'database' => getenv('DB_NAME'),
-        'username' => getenv('DB_USER'),
-        'password' => getenv('DB_PASS'),
-        'host' => getenv('DB_HOST'),
-        'port' => getenv('DB_PORT'),
-        'driver' => getenv('DB_DRIVER'),
-        'prefix' => getenv('DB_PREFIX'),
-        'collation' => getenv('DB_COLLATION'),
-    ];
+  $settings['hash_salt'] = 'YtvcheoGbURHzhaAADkcNGZPBSaK5RX1dIpRb0B-JsyJhjiBC3BB0jXJr9iEbFZgnSiIepw7ZA';
+  $dotenv = new Dotenv\Dotenv('../');
+  $dotenv->load();
+  $databases['default']['default'] = [
+    'database' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'host' => getenv('DB_HOST'),
+    'port' => getenv('DB_PORT'),
+    'driver' => getenv('DB_DRIVER'),
+    'prefix' => getenv('DB_PREFIX'),
+    'namespace' => getenv('DB_NAMESPACE'),
+    'collation' => getenv('DB_COLLATION'),
+  ];
 }
 
 /**
@@ -37,17 +41,8 @@ if (!isset($_ENV['PANTHEON_ENVIRONMENT'])) {
  */
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
-    include $local_settings;
+  include $local_settings;
 }
-$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'drupal',
-  'password' => 'drupal',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['install_profile'] = 'standard';
-$config_directories['sync'] = 'sites/default/files/config_xyAfBUYrfPhyUD_frk_Iu4DO7fDih8OwN_oTIo_5Vh2SpTKj_xanvmQfMKiAe-d2B_30j7gLgw/sync';
+
+
+
